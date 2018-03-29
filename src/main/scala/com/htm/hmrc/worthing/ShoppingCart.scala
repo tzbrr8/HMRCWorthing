0abcb767178bfa8691dead127dd8064cf8ddd7c7
HiteshMistryOodle <HiteshMistryOodle@users.noreply.github.com>
2018-03-29 17:29:01 +0100

    Add step 2

--- src/main/scala/com/htm/hmrc/worthing/ShoppingCart.scala
+++ src/main/scala/com/htm/hmrc/worthing/ShoppingCart.scala
@@ -2,15 +2,17 @@ package com.htm.hmrc.worthing
 
 class ShoppingCart {
 
-  type Pounds = Double
+  type Pounds = BigDecimal
 
   def checkout(items: List[String]): Pounds = {
 
     var total: Pounds = 0.00
+    var apples: Int = 0
+    var oranges: Int = 0
 
     items.foreach {
-      case "Apple" => total += 0.60
-      case "Orange" => total += 0.25
+      case "Apple" => apples += 1;  if(apples % 2 != 0) total += 0.60;
+      case "Orange" => oranges += 1; if(oranges % 3 != 0) total += 0.25;
       case _ => ()
     }
 
--- src/test/scala/com/htm/hmrc/worthing/ShoppingCartSpec.scala
+++ src/test/scala/com/htm/hmrc/worthing/ShoppingCartSpec.scala
@@ -19,19 +19,19 @@ class ShoppingCartSpec extends FlatSpec with Matchers{
   it should "take a list of 2 x Apple And 1 xOrange and Return the Total" in {
     val items = List( "Apple", "Orange", "Apple")
     val total = ShoppingCart(items)
-    total shouldBe 1.45
+    total shouldBe 0.85
   }
 
   it should "take a list of 2 x Apple And 2 xOrange and Return the Total" in {
     val items = List( "Apple", "Orange", "Apple", "Orange")
     val total = ShoppingCart(items)
-    total shouldBe 1.70
+    total shouldBe 1.10
   }
 
   it should "take a list of [Apple, Apple, Orange, Apple] and Return the Total" in {
     val items = List( "Apple", "Apple", "Orange", "Apple")
     val total = ShoppingCart(items)
-    total shouldBe 2.05
+    total shouldBe 1.45
   }
 
   it should "take a list of [] and Return the Total" in {
@@ -44,8 +44,60 @@ class ShoppingCartSpec extends FlatSpec with Matchers{
     //assumption that invalid items are ignored and binned.
     val items = List( "Apple", "Apple", "Orange", "BadApple")
     val total = ShoppingCart(items)
-    total shouldBe 1.45
+    total shouldBe 0.85
   }
+
+
+
+  it should "take list of just one apple and return half the total" in {
+    val items = List( "Apple")
+    val total = ShoppingCart(items)
+    total shouldBe 0.60
+  }
+
+  it should "take list of even apples and return half the total" in {
+    val items = List( "Apple", "Apple")
+    val total = ShoppingCart(items)
+    total shouldBe 0.60
+  }
+
+  it should "take list of even apples, more then one set and return half the total" in {
+    val items = List( "Apple", "Apple", "Apple", "Apple", "Apple", "Apple")
+    val total = ShoppingCart(items)
+    total shouldBe 1.80
+  }
+
+
+  it should "take list of just one Orange and return half the total" in {
+    val items = List( "Orange")
+    val total = ShoppingCart(items)
+    total shouldBe 0.25
+  }
+
+  it should "take list of even Oranges and return half the total" in {
+    val items = List( "Orange", "Orange")
+    val total = ShoppingCart(items)
+    total shouldBe 0.50
+  }
+
+  it should "take list of 3 Oranges and return price of 2 orranges" in {
+    val items = List( "Orange", "Orange", "Orange")
+    val total = ShoppingCart(items)
+    total shouldBe 0.50
+  }
+
+  it should "take list of multiple of 3 oranges and return 3 for price of 2" in {
+    val items = List( "Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+    val total = ShoppingCart(items)
+    total shouldBe 1.00
+  }
+
+  it should "take list of multiple of 2 apples and 3 oranges" in {
+    val items = List( "Apple", "Orange", "Apple", "Orange", "Orange")
+    val total = ShoppingCart(items)
+    total shouldBe 1.10
+  }
+
 
 
 }
